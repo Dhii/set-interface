35bca407676014d0509adc5655bb15b0efed27ee
Anton Ukhanev <xedin.unknown@gmail.com>
2016-09-22 14:30:49 +0200

    Added `SetInterface`

---
+++ src/SetInterface.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Something that can act as a set.
+ *
+ * @since [*next-version*]
+ */
+interface SetInterface extends Collection\CollectionInterface
+{
+    /**
+     * Adds an item to this set.
+     *
+     * @since [*next-version*]
+     *
+     * @param mixed $value The value to add.
+     */
+    public function add($value);
+
+    /**
+     * Removes the value from this set.
+     *
+     * @since [*next-version*]
+     *
+     * @param mixed $value The value to remove.
+     */
+    public function remove($value);
+
+    /**
+     * Determines whether this set contains the given value.
+     *
+     * @since [*next-version*]
+     *
+     * @param mixed $value The value to check for.
+     */
+    public function has($value);
+
+    /**
+     * Retrieves a list of all items in this set.
+     *
+     * @since [*next-version*]
+     *
+     * @return mixed[]|\Traversable
+     */
+    public function items();
+}

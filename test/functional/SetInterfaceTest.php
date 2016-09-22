01e7dd8bcd570813d99716a0422ac283445cbb79
Anton Ukhanev <xedin.unknown@gmail.com>
2016-09-22 14:45:40 +0200

    Test added

---
+++ test/functional/SetInterfaceTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Dhii\Set\FuncTest;
+
+/**
+ * Tests {@see \Dhii\Set\SetInterface}
+ *
+ * @since [*next-version*]
+ */
+class SetInterfaceTest extends \Xpmock\TestCase
+{
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return \Dhii\Set\SetInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock('Dhii\Set\SetInterface')
+                ->add()
+                ->remove()
+                ->has()
+                ->items()
+                ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of an implementing class can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf('Dhii\Set\SetInterface', $subject, 'Subject is not a valid set');
+    }
+}
